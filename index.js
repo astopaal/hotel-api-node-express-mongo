e8ee59d6edb7f8aa48655060ecc22b1e6b6dc190
Samet Topal <astopaal@gmail.com>
2023-03-05 17:13:01 +0300

    create dbhelper

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
-
+const db = require('./config/db.js')
 dotenv.config();
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cookieParser());
-
+db();
 const PORT = 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
